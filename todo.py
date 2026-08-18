8e73291e7f7c5c68fedc0e94cdf162878e8f36f5
goutamgulati405-lab <goutamgulati405@gmail.com>
2026-08-18 10:25:33 +0530

    Fix formatting issues in todo.py

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-import streamlit as at
+import streamlit as st
 
 st.title("📝 TaskMate")
 st.subheader("🤝 To-Do List")
@@ -18,14 +18,12 @@ st.subheader("📋 My Tasks")
 
 for i, task in enumerate(st.session_state.tasks):
     col1, col2 = st.columns([4, 1])
-
     with col1:
         st.write(f"☐ {task}")
-
     with col2:
         if st.button("Delete", key=i):
             st.session_state.tasks.pop(i)
             st.rerun()
 
 st.divider()
-st.caption("💻 Created by Goutam Gulati | BCA Student"
+st.caption("💻 Created by Goutam Gulati | BCA Student")
